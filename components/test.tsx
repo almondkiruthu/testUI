5a293ec499ab30450dab2345f03d1d150cccaeab
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-07-05 23:01:13 +0300

    Modify  the button to test it's functionality

--- components/test.tsx
+++ components/test.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button';
 
 export default function Test() {
   const print = () => {
-    console.log('Hello world');
+    console.log('123 on Stateful component');
   };
   return <Button onClick={print} className="">Click me</Button>;
 }
